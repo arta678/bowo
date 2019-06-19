0df6f4a2733ed638e335f3f61eae132f73caf4d8
arta678 <artawgn678@gmail.com>
2019-06-19 20:11:40 +0800

    fix

--- bowo.sh
+++ bowo.sh
@@ -26,13 +26,13 @@ main(){
 	echo "==========================";
 	#sudo airmon-ng;
 	cekInterface;
-	echo -e "========================== $Cafe";
+	echo -e "$white========================== $Cafe";
 	echo -e "1. Hidupkan Monitor";  
 	echo -e "2. Matikan Monitor";  
 	echo -e "3. Status Monitor"; 
 	echo -e "4. Scan Sekitar";
 	echo -e "5. Lumpuhkan <samping>";
-	echo -e "0. Keluar \033[1;m"; 
+	echo -e "0. Keluar $white"; 
 	echo "==========================";
 	read  -p "Pilih  : " a
 	if [[ $a == 1 ]]; then
@@ -46,7 +46,7 @@ main(){
 		main;
 
 	elif [[ $a == 4 ]]; then
-		sudo airodump-ng mon0;
+		# sudo airodump-ng mon0;
 		pilihScan;
 	elif [[ $a == 5 ]]; then
 		bunuh;
@@ -106,30 +106,43 @@ serang(){
 }
 
 pilihScan(){
-	echo "";
-	echo  "1 = BUjANGRESIDENCE";
-	echo  "2 = BUjANGRESIDENCE_plus";
-	echo  "3 = SoffA";
-	read  -p "Pilih  : " pilih
-	if [[ $pilih == 1 ]]; then
-		target="98:DE:D0:BD:0F:E0";
-	elif [[ $pilih == 2 ]]; then
-		target="50:64:2B:25:F8:AB";
-	elif [[ $pilih == 3 ]]; then
-		target="34:CE:00:7A:8D:60";
-    fi
-	sudo airodump-ng --bssi $target mon0
-	
-	read  -p "Ingin Melakukan Serangan? (Y/N) : " serang
-	if [[ $serang == y ]]; then
-		serang;
-	elif [[ $serang == Y ]]; then
-		serang;
-	elif [[ $serang == N ]]; then
-		main;
-	elif [[ $serang == n ]]; then
-		main;
+	t1=$(ifconfig | grep -o mon0)
+	t2='mon0'
+
+	if [ "$t1" = "$t2" ]; then
+	  	sudo airodump-ng mon0;
+	  	echo "";
+		echo  "1 = BUjANGRESIDENCE";
+		echo  "2 = BUjANGRESIDENCE_plus";
+		echo  "3 = SoffA";
+		read  -p "Pilih  : " pilih
+		if [[ $pilih == 1 ]]; then
+			target="98:DE:D0:BD:0F:E0";
+		elif [[ $pilih == 2 ]]; then
+			target="50:64:2B:25:F8:AB";
+		elif [[ $pilih == 3 ]]; then
+			target="34:CE:00:7A:8D:60";
+	    fi
+		sudo airodump-ng --bssi $target mon0
+		
+		read  -p "Ingin Melakukan Serangan? (Y/N) : " serang
+		if [[ $serang == y ]]; then
+			serang;
+		elif [[ $serang == Y ]]; then
+			serang;
+		elif [[ $serang == N ]]; then
+			main;
+		elif [[ $serang == n ]]; then
+			main;
+		fi
+	else
+	  	echo -e "$red Maaf Monito belum Aktif \033[1;m";
+	  	echo -e "$red Untuk mengaktifkan teken 1 pada menu $white";
+	  	echo ""
+	  	read -p "Tekan enter untuk ke menu"
+	  	main
 	fi
+	
 }
 
 cekInterface(){
@@ -143,19 +156,6 @@ cekInterface(){
 	fi
 }
 
-#color
-white="\033[1;37m"
-grey="\033[0;37m"
-purple="\033[0;35m"
-red="\033[1;31m"
-green="\033[1;32m"
-yellow="\033[1;33m"
-Purple="\033[0;35m"
-Cyan="\033[0;36m"
-Cafe="\033[0;33m"
-Fiuscha="\033[0;35m"
-blue="\033[1;34m"
-transparent="\e[0m"
 #mengecek user
 if [[ $EUID -ne 0 ]]; then
    echo "<<< Kamu harus dalam keadaan root!>>>" 
@@ -167,17 +167,3 @@ main;
 
 
 
-
-#color
-white="\033[1;37m"
-grey="\033[0;37m"
-purple="\033[0;35m"
-red="\033[1;31m"
-green="\033[1;32m"
-yellow="\033[1;33m"
-Purple="\033[0;35m"
-Cyan="\033[0;36m"
-Cafe="\033[0;33m"
-Fiuscha="\033[0;35m"
-blue="\033[1;34m"
-transparent="\e[0m"
